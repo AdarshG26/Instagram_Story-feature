6f828ec4e758275ada8792243e15207bd02042ad
AdarshG26 <adarshg2612@gmail.com>
2024-09-29 10:35:09 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Instagram story feature, when you click on story it will appear on screen and after 3 seconds it will disappear
---
+++ script.js
@@ -0,0 +1,41 @@
+var arr = [
+    {
+        dp: "https://images.unsplash.com/photo-1524710800377-5cdd93fa491a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://plus.unsplash.com/premium_photo-1677993185892-f7823f314c4c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://plus.unsplash.com/premium_photo-1686730540277-c7e3a5571553?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1541348263662-e068662d82af?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1541348263662-e068662d82af?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1726502426120-942a744df0d3?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1648594756894-151b3eb89400?q=80&w=2003&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1671418203939-c54f3ec6f9f1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1713915653220-d744cd054115?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1633651400570-b63df5f8709a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
+
+var clutter = ""
+arr.forEach(function(elem, index){
+    clutter += `<div class="story">
+    <img id="${index}" src="${elem.dp}" alt="ERROR:(">
+    </div>`
+})
+
+document.querySelector('.stories').innerHTML = clutter
+
+document.querySelector('.stories').addEventListener("click", function(dets){
+    // arr[dets.target.id].story
+    document.querySelector('.full-screen').style.display = "block"
+    document.querySelector('.full-screen').style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+    setTimeout(function(){
+        document.querySelector('.full-screen').style.display = "none"
+    }, 3000)
+})
